7aeef1e4ec6363662e31f172c913640e3b252b06
Nicholas Beninato <26497019+beninato8@users.noreply.github.com>
2025-12-07 23:46:46 -0800

    day 5

--- 2025/day-05/cafeteria.ts
+++ 2025/day-05/cafeteria.ts
@@ -2,7 +2,7 @@
 
 import { readFileSync } from "fs";
 
-type FileInput = [number[], number[]];
+type FileInput = [number[][], number[]];
 
 // given input in a file, split into ranges, get actual numbers, and flatten
 function parseFile(path: string): FileInput {
@@ -11,7 +11,7 @@ function parseFile(path: string): FileInput {
   const sections = fileContent
     .split("\n\n")
     .map((section) => section.split("\n"));
-  const ranges = sections[0].flatMap((line) =>
+  const ranges = sections[0].map((line) =>
     line.split("-").map((num) => parseInt(num)),
   );
   const ingredients = sections[1].map((id) => parseInt(id));
@@ -19,16 +19,62 @@ function parseFile(path: string): FileInput {
   return [ranges, ingredients];
 }
 
-function addNewRange(ranges: number[], newRange: number[]): number[] {
-  return ranges;
+// merge overlapping ranges
+function mergeRanges(ranges: number[][]): number[][] {
+  const newRanges: number[][] = [];
+  // sort by starting value
+  ranges = ranges.sort((a, b) => a[0] - b[0]) 
+  for (let i = 0; i < ranges.length; i++) {
+    let range = ranges[i];
+    if (newRanges.length == 0) {
+      newRanges.push(range)
+    } else {
+      let prevRange = newRanges[newRanges.length - 1]
+      let [prevStart, prevEnd] = prevRange;
+      let [start, end] = range;
+      if (start <= prevEnd) {
+        newRanges[newRanges.length - 1][1] = Math.max(prevEnd, end)
+      } else {
+        newRanges.push(range)
+      }
+    }
+  }
+  return newRanges;
 }
 
+function isInRange(ranges:number[][], value: number): boolean {
+  for (let i = 0; i < ranges.length; i++) {
+    const [start, end] = ranges[i]
+    if (value >= start && value <= end) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// if part 1, we want count of fresh ingredients
+// if part 2, we want all possible fresh ingredients in range
 function getTotal(
-  ranges: number[],
+  ranges: number[][],
   ingredients: number[],
   isPart2 = false,
 ): number {
-  return 0;
+  console.log(ranges)
+  ranges = mergeRanges(ranges);
+  console.log(ranges)
+  if (!isPart2) {
+    const freshIngredients = ingredients.filter((value) => isInRange(ranges, value))
+
+    return freshIngredients.length
+  } else {
+    let result = 0;
+    for (let i = 0; i < ranges.length; i++) {
+      const [start, end] = ranges[i]
+      result += end - start + 1
+    }
+    return result
+  }
+
 }
 
 function main() {
@@ -40,9 +86,9 @@ function main() {
 
   const tests: TestData[] = [
     ["example.txt", false, 3],
-    // ["input.txt", false, 1480],
-    // ["example.txt", true, 43],
-    // ["input.txt", true, 8899],
+    ["input.txt", false, 529],
+    ["example.txt", true, 14],
+    ["input.txt", true, 344260049617193],
   ];
 
   for (const [path, isPart2, expected] of tests) {
